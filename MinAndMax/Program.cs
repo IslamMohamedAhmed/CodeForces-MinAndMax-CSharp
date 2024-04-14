9b5778618d3a077d237ed6a5cbda7f0241ba5d3b
unknown <islammohamedahmed49@gmail.com>
2024-04-14 14:09:43 +0200

    mam

---
+++ MinAndMax/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+int n, mx = 0, mn = 100005 ;
+int[] arr1 = new int[100005];
+n = ToInt32(ReadLine());
+string str1 = ReadLine();
+var arr2 = str1.Split(" ");
+for(int i = 0; i < n; i++)
+{
+    arr1[i] = ToInt32(arr2[i]);
+    mn = Min(mn, arr1[i]);
+    mx = Max(mx, arr1[i]);
+}
+
+WriteLine($"{mn} {mx}");
+
